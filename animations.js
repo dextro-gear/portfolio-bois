c9a44f6e261dbbb309278910d8a2b908e4c4f80d
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2020-10-15 12:23:15 +0530

    added animations

--- animations.js
+++ animations.js
@@ -1,4 +1,7 @@
 import "./node_modules/jquery/dist/jquery.js";
+import "./node_modules/smooth-scrollbar/dist/smooth-scrollbar.js";
+
+var Scrollbar = window.Scrollbar;
 
 var win = $(window);
 var circleThingy = $(".srini-circle");
@@ -6,3 +9,5 @@ var circleThingy = $(".srini-circle");
 win.on("scroll", ()=>{
     circleThingy.css("transform","rotate(" + (win.scrollTop() * 0.2) +"deg)" );
 })
+
+Scrollbar.init(document.querySelector("body"));
