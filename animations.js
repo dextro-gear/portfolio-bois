6205e654167b2d6eaf0b92645cdb281c47d035e6
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2020-10-15 12:31:43 +0530

    debug animations

--- animations.js
+++ animations.js
@@ -1,7 +1,7 @@
 import "./node_modules/jquery/dist/jquery.js";
-import "./node_modules/smooth-scrollbar/dist/smooth-scrollbar.js";
+// import "./node_modules/smooth-scrollbar/dist/smooth-scrollbar.js";
 
-var Scrollbar = window.Scrollbar;
+// var Scrollbar = window.Scrollbar;
 
 var win = $(window);
 var circleThingy = $(".srini-circle");
@@ -10,4 +10,4 @@ win.on("scroll", ()=>{
     circleThingy.css("transform","rotate(" + (win.scrollTop() * 0.2) +"deg)" );
 })
 
-Scrollbar.init(document.querySelector("body"));
+// Scrollbar.init(document.querySelector("body"));
