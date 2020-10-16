e1875c18dc47fd243f54968df97fb4cf288b8638
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2020-10-16 19:28:47 +0530

    debug text-container opacity animation

--- animations.js
+++ animations.js
@@ -1,11 +1,28 @@
 // import Scrollbar from "smooth-scrollbar";
 
+const { contains } = require("jquery");
+
 window.addEventListener("scroll", ()=>{
     var scroll = window.scrollY;
     var circleThingy = document.querySelector(".srini-circle");
 
     circleThingy.style.transform = "rotate(" + (scroll*0.2) + "deg)";
 })
+
+var textLayout = document.querySelectorAll(".work-layout");
+
+textLayout.forEach((container)=> {
+    var workText = container.querySelector(".work-text-container");
+
+    container.addEventListener("mouseover", ()=>{
+        workText.style.opacity = 0.4;
+    }, false);
+
+    container.addEventListener("mouseleave", ()=>{
+        workText.style.opacity = 1;
+    }, false);
+});
+
 
 // Scrollbar.init(document.querySelector("html"), {
 //     "damping": 0.5,
