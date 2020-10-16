84fff330b289216b5f5e96f7c2cdbe44f03a52e6
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2020-10-16 19:06:17 +0530

    debug circle spin scroll animation

--- animations.js
+++ animations.js
@@ -1,9 +1,12 @@
-import Scrollbar from "smooth-scrollbar";
+// import Scrollbar from "smooth-scrollbar";
 
-document.addEventListener("scroll", ()=>{
-    console.log(window.scrollY())
+window.addEventListener("scroll", ()=>{
+    var scroll = window.scrollY;
+    var circleThingy = document.querySelector(".srini-circle");
+
+    circleThingy.style.transform = "rotate(" + (scroll*0.2) + "deg)";
 })
 
-Scrollbar.init(document.querySelector("html"), {
-    "damping": 0.5,
-});
+// Scrollbar.init(document.querySelector("html"), {
+//     "damping": 0.5,
+// });
