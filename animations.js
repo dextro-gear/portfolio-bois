5e3746ddedebc0bbb717d5465362e9a653c50c9c
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2020-10-16 18:08:08 +0530

    add scrollbar

--- animations.js
+++ animations.js
@@ -1,13 +1,9 @@
-import "./node_modules/jquery/dist/jquery.js";
-// import "./node_modules/smooth-scrollbar/dist/smooth-scrollbar.js";
+import Scrollbar from "smooth-scrollbar";
 
-// var Scrollbar = window.Scrollbar;
-
-var win = $(window);
-var circleThingy = $(".srini-circle");
-
-win.on("scroll", ()=>{
-    circleThingy.css("transform","rotate(" + (win.scrollTop() * 0.2) +"deg)" );
+document.addEventListener("scroll", ()=>{
+    console.log(window.scrollY())
 })
 
-// Scrollbar.init(document.querySelector("body"));
+Scrollbar.init(document.querySelector("html"), {
+    "damping": 0.5,
+});
