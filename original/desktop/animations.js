832945190c64a3f8acfdda9f3c26eb6a1991e765
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2020-12-11 21:27:04 +0530

    directory cleanup and add minified version

---
+++ original/desktop/animations.js
@@ -0,0 +1,76 @@
+
+/* CIRCLE THINGY SCROLL ROTATE ANIMATION */
+window.addEventListener("scroll", ()=>{
+    var scroll = window.scrollY;
+    var circleThingy = document.querySelector(".srini-circle");
+
+    circleThingy.style.transform = "rotate(" + (scroll*0.2) + "deg)";
+})
+/* CIRCLE THINGY SCROLL ROTATE ANIMATION */
+
+
+
+/* WORK THUMBNAIL OPACITY ANIMATION */
+var textLayout = document.querySelectorAll(".work-layout");
+
+textLayout.forEach((container)=> {
+    var workText = container.querySelector(".work-text-container");
+    var workImage = container.querySelector(".work-image");
+
+    workImage.addEventListener("mouseover", ()=>{
+        workText.style.opacity = 0.4;
+    }, false);
+
+    workImage.addEventListener("mouseleave", ()=>{
+        workText.style.opacity = 1;
+    }, false);
+});
+/* WORK THUMBNAIL OPACITY ANIMATION */
+
+
+
+/* TRAVIS HOVER ANIMATION */
+const travisLink = document.querySelector(".travis-link");
+
+travisLink.addEventListener("mouseover", e=>{
+    var travisImage = document.querySelector(".travis");
+    travisImage.setAttribute("style", "top:" + e.pageY + "px; left:" + e.pageX + "px; transform: translate(-50%, -50%);");
+    travisImage.style.display = "initial";
+    console.log("mouseover");
+}, false);
+
+travisLink.addEventListener("mouseleave", e=>{
+    var travisImage = document.querySelector(".travis");
+    travisImage.style.display = "none";
+    console.log("mouseleave");
+}, false);
+/* TRAVIS HOVER ANIMATION */
+
+
+
+/* CUSTOM CURSOR */
+// window.addEventListener("mousemove", event=>{
+//     var cursor = document.querySelector(".cursor");
+//     cursor.style.top = event.pageY + "px";
+//     cursor.style.left = event.pageX + "px";
+// }, false);
+/* CUSTOM CURSOR */
+
+
+
+/* VIEW CURSOR */
+// var viewCursor = $(".view-cursor");
+// var workImage = $(".work-image");
+
+// $(document).on("mousemove", (event)=>{
+//     viewCursor.css("top", event.pageY + "px");
+//     viewCursor.css("left", event.pageX + "px");
+// })
+// workImage.on("mouseenter", ()=>{
+//     viewCursor.css("transform", "scale(1)");
+// });
+
+// workImage.on("mouseleave", ()=>{
+//     viewCursor.css("transform", "scale(0)");
+// });
+/* VIEW CURSOR */
---
+++ original/mobile/animations.js
@@ -0,0 +1,51 @@
+// import anime from './node_modules/animejs/lib/anime.es.js';
+import anime from "https://unpkg.com/animejs@3.2.1/lib/anime.es.js?module";
+
+var page = document.querySelector("html");
+
+// NAV OVERLAY ANIMATION
+var overlay = document.querySelector(".overlay-body");
+console.log(overlay);
+
+var slideUpAnimation = anime({
+    targets: overlay,
+    translateY: ["0%", "-100%"],
+    duration: 500,
+    easing: "easeInOutExpo",
+    autoplay: false,
+});
+
+var dropDownAnimation = anime({
+    targets: overlay,
+    translateY: ["-100%", "0%"],
+    duration: 500,
+    easing: "easeInOutExpo",
+    autoplay: false,
+});
+
+var hamburger = document.querySelector(".hamburger");
+var closeButton = document.querySelector(".close-button");
+
+hamburger.addEventListener("click", ()=>{
+    dropDownAnimation.play();
+    page.style.overflowY = "hidden";
+    console.log("Open");
+
+}, false);
+
+closeButton.addEventListener("click", ()=>{
+    slideUpAnimation.play();
+    page.style.overflowY = "auto";
+    console.log("Close");
+
+}, false);
+// NAV OVERLAY ANIMATION
+
+
+// circle rotate animation
+var circleThingy = document.querySelector(".circle-thingy");
+
+window.addEventListener("scroll", ()=>{
+    circleThingy.style.transform = " rotate(-" + (window.scrollY * 0.3) + "deg)";
+}, false);
+// circle rotate animation
